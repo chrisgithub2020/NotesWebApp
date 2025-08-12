cdf9896befeb97232dc107f7317dce157086ae89
chrisgithub2020 <agyemanchris0@gmail.com>
2025-08-12 13:10:01 +0000

    seeing something

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ const typeDefinitions = gql (
 // const client = new MongoClient('mongodb://127.0.0.1:27017/NotesApp')
 const client = new MongoClient(uri)
 const PORT = parseInt(process.env.PORT || '4000', 10)
-
+console.log(uri)
 async function startServer() {
     await client.connect()
     const server = new ApolloServer({typeDefs: typeDefinitions, resolvers: resolvers})
